29db1405695fd51c0ecf127d71de6d25ba1fa256
Glitch (jefferylgraham--metric-imperial-converter) <none>
2018-11-17 02:59:16 +0000

    🏨🍈 Updated with Glitch

---
+++ controllers/convertHandler.js
@@ -0,0 +1,67 @@
+/*
+*
+*
+*       Complete the handler logic below
+*       
+*       
+*/
+
+function ConvertHandler() {
+  
+  this.getNum = function(input) {
+    if (input) {
+      var firstChar = input.match('[a-zA-Z]');
+      var index = input.indexOf(firstChar);
+
+      var number = input.substring(0, index);
+
+      var re = /\//ig;
+      var found = (number.match(/\//g) || []).length;
+      if (found > 1) {
+        return 'invalid number';
+      }
+      else {
+        return number;
+      }
+    }
+    else {
+      return 1;
+    }
+  };
+  
+  this.getUnit = function(input) {
+    var result;
+    
+    return result;
+  };
+  
+  this.getReturnUnit = function(initUnit) {
+    var result;
+    
+    return result;
+  };
+
+  this.spellOutUnit = function(unit) {
+    var result;
+    
+    return result;
+  };
+  
+  this.convert = function(initNum, initUnit) {
+    const galToL = 3.78541;
+    const lbsToKg = 0.453592;
+    const miToKm = 1.60934;
+    var result;
+    
+    return result;
+  };
+  
+  this.getString = function(initNum, initUnit, returnNum, returnUnit) {
+    var result;
+    
+    return result;
+  };
+  
+}
+
+module.exports = ConvertHandler;
